3dd2963b7dd64088fc65d4f904c2fb3fc4d57517
VZachary13 <vinicius.zachary@gmail.com>
2024-05-26 16:44:35 -0700

    added basic layout of pages

--- React Portfolio/src/main.jsx
+++ React Portfolio/src/main.jsx
@@ -1,10 +1,45 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import ReactDOM from "react-dom/client";
+// Bringing in the required imports from 'react-router-dom' to set up application routing behavior
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+import "bootstrap/dist/css/bootstrap.min.css";
+
+// Bringing in the pages the router will use to conditionally show the appropriate views
+import App from "./App";
+import Error from "./pages/Error";
+import About from "./pages/About";
+import Contact from "./pages/Contact";
+import Project from "./pages/Project";
+import Portfolio from "./pages/Portfolio";
+
+// Define the accessible routes, and which components respond to which URL
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />,
+    errorElement: <Error />,
+    children: [
+      {
+        index: true,
+        element: <About />,
+      },
+      {
+        path: "portfolio",
+        element: <Portfolio />,
+      },
+      {
+        path: "contact",
+        element: <Contact />,
+      },
+      {
+        path: "project/:id",
+        element: <Project />,
+      },
+    ],
+  },
+]);
+
+// Render the RouterProvider component
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <RouterProvider router={router} />
+);
