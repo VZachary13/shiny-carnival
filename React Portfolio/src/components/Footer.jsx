109be5333a569ae8e093f2d0d509da51ff96952f
VZachary13 <vinicius.zachary@gmail.com>
2024-05-28 17:46:23 -0700

    adjusted css for portfolio page

---
+++ React Portfolio/src/components/Footer.jsx
@@ -0,0 +1,7 @@
+export default function Footer() {
+  return (
+    <footer>
+      <p>Made with L0V3 by Vini Z.</p>
+    </footer>
+  );
+}
--- React Portfolio/src/components/UI/Project.jsx
+++ React Portfolio/src/components/UI/Project.jsx
@@ -2,15 +2,19 @@ export default function Project({ project }) {
   const ifDeployed = <a href={project.deployedLink}>Deployed Application</a>;
   return (
     <div className="projectCard">
-      <h2>{project.name}</h2>
-      <img src={project.image} alt={`${project.name} screenshot`} />
-      <div className="linkDiv">
-        <a href={project.githubRepo}>GitHub Repository</a>
-        {project.deployedLink !== "Not Deployed" ? (
-          ifDeployed
-        ) : (
-          <p>{project.deployedLink}</p>
-        )}
+      <div className="projectCardContainer">
+        <img src={project.image} alt={`${project.name} screenshot`} />
+        <div className="layer">
+          <h2>{project.name}</h2>
+          <div className="linkDiv">
+            <a href={project.githubRepo}>GitHub Repository</a>
+            {project.deployedLink !== "Not Deployed" ? (
+              ifDeployed
+            ) : (
+              <p>{project.deployedLink}</p>
+            )}
+          </div>
+        </div>
       </div>
     </div>
   );
